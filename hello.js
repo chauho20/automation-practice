9e84c15b0abf3e00bce544e95e83dfe4e793728d
Chau Ho <hothichau19520@gmail.com>
2026-03-30 20:15:43 +0700

    Add function calculateAge

--- hello.js
+++ hello.js
@@ -5,6 +5,11 @@ function sayHello(name) {
  function sayGoodbye(name) {
   return "Goodbye, " + name + "!";
  }
+
+ function calculateAge(year) {
+  return "I am " + (2026 - year) + " years old.";
+ }
  
  console.log(sayHello("Automation Tester"));
  console.log(sayGoodbye("Manual Testing"));
+ console.log(calculateAge(2000));
